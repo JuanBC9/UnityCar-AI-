4ee2de64c85da0ebc88d387d5848b52b333fa6f8
David Andrés <skyvidas@gmail.com>
2021-06-18 21:42:40 +0200

    assets analogicos actualizados

--- Assets/BEST_IA_BITCH/Sensors_Digital.cs
+++ Assets/BEST_IA_BITCH/Sensors_Digital.cs
@@ -63,7 +63,7 @@ public class Sensors_Digital : MonoBehaviour
             File.AppendAllText(path, str);
         }
 
-        Debug.Log(str);
+        //Debug.Log(str);
     }
 
     private void GetSensorData()
@@ -102,15 +102,46 @@ public class Sensors_Digital : MonoBehaviour
 
     public double SteeringInputCalculator()
     {
-        double steeringValueD0 = -0.8852850206419234 + 1.896534903668094 * sensorData[0] + -1.2652277071815028 * sensorData[1] + -0.2010457856672807 * sensorData[2] + -0.30006409313620575 * sensorData[3] + 0.48756994117342245 * sensorData[4];
-        double steeringValueD1 = -0.14192381048026328 * Mathf.Pow(sensorData[0], 1) + 0.06793867086215136 * Mathf.Pow(sensorData[1], 1) + 0.01797455949552655 * Mathf.Pow(sensorData[2], 1) + 0.04461746954713519 * Mathf.Pow(sensorData[3], 1) + -0.07351089716524958 * Mathf.Pow(sensorData[4], 1);
-        double steeringValueD2 =  0.0053242933453843694 * Mathf.Pow(sensorData[0], 2) + -0.0018026063532814646 * Mathf.Pow(sensorData[1], 2) + -0.0008302299113159112 * Mathf.Pow(sensorData[2], 2) + -0.0024327270159660323 * Mathf.Pow(sensorData[3], 2) + 0.00473797887233356 * Mathf.Pow(sensorData[4], 2);
-        double steeringValueD3 =  0.0053242933453843694 * Mathf.Pow(sensorData[0], 3) + -0.0018026063532814646 * Mathf.Pow(sensorData[1], 3) + -0.0008302299113159112 * Mathf.Pow(sensorData[2], 3) + -0.0024327270159660323 * Mathf.Pow(sensorData[3], 3) + 0.00473797887233356 * Mathf.Pow(sensorData[4], 3);
-        double steeringValueD4 = -0.00010615952868219817 * Mathf.Pow(sensorData[0], 4) + 0.00002330094647753101 * Mathf.Pow(sensorData[1], 4) + 0.000021639391214862223 * Mathf.Pow(sensorData[2], 4) + 0.00006362332309231551 * Mathf.Pow(sensorData[3], 4) + -0.0001493966754550788 * Mathf.Pow(sensorData[4], 4);
-        double steeringValueD5 =  00001072637715728868 * Mathf.Pow(sensorData[0], 5) + -0.00000010779567847059445 * Mathf.Pow(sensorData[1], 5) + -0.0000003009253472336802 * Mathf.Pow(sensorData[2], 5) + -0.0000008096545534022186 * Mathf.Pow(sensorData[3], 5) + 0.0000022755767607246166 * Mathf.Pow(sensorData[4], 5);
-        double steeringValueD6 = -0.000000004317716224530317 * Mathf.Pow(sensorData[0], 6) + -0.00000000016362881748004007 * Mathf.Pow(sensorData[1], 6) + 0.000000001726454421608149 * Mathf.Pow(sensorData[2], 6) + 0.000000004048355521366342 * Mathf.Pow(sensorData[3], 6) + -0.000000013391490434040537 * Mathf.Pow(sensorData[4], 6);
+        double steeringValueD0 = -0.8852850206419234;
+
+        double steeringValueD1 = 1.896534903668094 * sensorData[0]
+                                -1.2652277071815028 * sensorData[1]
+                                -0.2010457856672807 * sensorData[2]
+                                -0.30006409313620575 * sensorData[3]
+                                + 0.48756994117342245 * sensorData[4];
+
+        double steeringValueD2 = -0.14192381048026328 * Mathf.Pow(sensorData[0], 2) 
+                                + 0.06793867086215136 * Mathf.Pow(sensorData[1], 2)
+                                + 0.01797455949552655 * Mathf.Pow(sensorData[2], 2)
+                                + 0.04461746954713519 * Mathf.Pow(sensorData[3], 2)
+                                - 0.07351089716524958 * Mathf.Pow(sensorData[4], 2);
+
+        double steeringValueD3 =  0.0053242933453843694 * Mathf.Pow(sensorData[0], 3)
+                                - 0.0018026063532814646 * Mathf.Pow(sensorData[1], 3)
+                                - 0.0008302299113159112 * Mathf.Pow(sensorData[2], 3)
+                                - 0.0024327270159660323 * Mathf.Pow(sensorData[3], 3)
+                                + 0.00473797887233356 * Mathf.Pow(sensorData[4], 3);
+
+        double steeringValueD4 = -0.00010615952868219817 * Mathf.Pow(sensorData[0], 4)
+                                + 0.00002330094647753101 * Mathf.Pow(sensorData[1], 4)
+                                + 0.000021639391214862223 * Mathf.Pow(sensorData[2], 4)
+                                + 0.00006362332309231551 * Mathf.Pow(sensorData[3], 4)
+                                - 0.0001493966754550788 * Mathf.Pow(sensorData[4], 4);
+
+        double steeringValueD5 =  0.000001072637715728868 * Mathf.Pow(sensorData[0], 5)
+                                - 0.00000010779567847059445 * Mathf.Pow(sensorData[1], 5)
+                                - 0.0000003009253472336802 * Mathf.Pow(sensorData[2], 5)
+                                - 0.0000008096545534022186 * Mathf.Pow(sensorData[3], 5)
+                                + 0.0000022755767607246166 * Mathf.Pow(sensorData[4], 5);
+
+        double steeringValueD6 = -0.000000004317716224530317 * Mathf.Pow(sensorData[0], 6)
+                                - 0.00000000016362881748004007 * Mathf.Pow(sensorData[1], 6)
+                                + 0.000000001726454421608149 * Mathf.Pow(sensorData[2], 6)
+                                + 0.000000004048355521366342 * Mathf.Pow(sensorData[3], 6)
+                                - 0.000000013391490434040537 * Mathf.Pow(sensorData[4], 6);
 
         double steeringValueTotal = steeringValueD0 + steeringValueD1 + steeringValueD2 + steeringValueD3 + steeringValueD4 + steeringValueD5 + steeringValueD6;
+        Debug.LogWarning(steeringValueTotal);
         return steeringValueTotal;
     }
 }
