f5f2c5b46f436d24ede1dee597be97819021708c
JotaBeCe <9juanbc9@gmail.com>
2021-06-18 16:47:51 +0200

    Dataset inicial creado

--- Assets/Sensors.cs
+++ Assets/Sensors.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Sensors : MonoBehaviour
 {
@@ -15,8 +16,17 @@ public class Sensors : MonoBehaviour
 
     RaycastHit raycastHit;
 
+    string path;
+
     private void Start()
     {
+        path = Application.dataPath + "/Data.txt";
+
+        if (!File.Exists(path))
+        {
+            File.WriteAllText(path, "x1 x2 x3 x4 label\n");
+        }
+
         sensorData = new float[5];
         sensorVectors = new Vector3[5];
         endPoints = new Vector3[5];
@@ -28,6 +38,10 @@ public class Sensors : MonoBehaviour
         UpdateVectors();
         DrawSensors();
         GetSensorData();
+    }
+
+    private void LateUpdate()
+    {
         printSensorData();
     }
 
@@ -36,7 +50,25 @@ public class Sensors : MonoBehaviour
         string str = "";
         foreach (float data in sensorData)
         {
-            str += data + ", ";
+            str += data + " ";
+        }
+
+        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
+        {
+            str += "STEER_LEFT";
+        }
+        else if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
+        {
+            str += "STEER_RIGHT";
+        } else 
+        {
+            str += "NO_STEERING";
+        }
+
+        if (File.Exists(path))
+        {
+            str += "\n";
+            File.AppendAllText(path, str);
         }
 
         Debug.Log(str);
@@ -49,12 +81,12 @@ public class Sensors : MonoBehaviour
             if (Physics.Raycast(this.transform.position, sensorVectors[i], out raycastHit, maxSensorDist))
             {
                 sensorData[i] = raycastHit.distance;
+                endPoints[i] = raycastHit.point;
             } else
             {
                 sensorData[i] = maxSensorDist;
+                endPoints[i] = sensorVectors[i].normalized * maxSensorDist;
             }
-
-            endPoints[i] = raycastHit.point;
         }
     }
 
@@ -68,10 +100,10 @@ public class Sensors : MonoBehaviour
 
     private void UpdateVectors()
     {
-        sensorVectors[0] = transform.forward - (Vector3.forward * .4f);
-        sensorVectors[1] = transform.forward - (Vector3.forward * .2f);
+        sensorVectors[0] = Quaternion.AngleAxis(-20f, Vector3.up) * transform.forward;
+        sensorVectors[1] = Quaternion.AngleAxis(-10f, Vector3.up) * transform.forward;
         sensorVectors[2] = transform.forward;
-        sensorVectors[3] = transform.forward + (Vector3.forward * .2f);
-        sensorVectors[4] = transform.forward + (Vector3.forward * .4f);
+        sensorVectors[3] = Quaternion.AngleAxis(10f, Vector3.up) * transform.forward;
+        sensorVectors[4] = Quaternion.AngleAxis(20f, Vector3.up) * transform.forward;
     }
 }
