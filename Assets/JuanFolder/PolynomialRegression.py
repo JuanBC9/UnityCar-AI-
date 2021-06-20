4cf88d2e58803c2f25d812dbc3f6d0384371a249
JotaBeCe <9juanbc9@gmail.com>
2021-06-20 17:53:44 +0200

    PolynomialRegression Implementado

--- Assets/JuanFolder/PolynomialRegression.py
+++ Assets/JuanFolder/PolynomialRegression.py
@@ -1,4 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import linear_model
+
+class PolynomialRegression:
+
+    def __init__(self,degree, Data):
+        self.degree = degree
+        self.regr = linear_model.LinearRegression()
+        self.dataset = pd.read_csv(Data, sep=" ")
+
+        XValues = dataset.iloc[:,0:-1].values
+        
+        newX = np.zeros(XValues.shape)
+
+        for i in range(2, self.degree + 1):
+            newX = np.concatenate((newX, XValues**i), axis = 1)
+
+        self.X = newX
+
+        self.y = dataset.iloc[:,-1:].values
+
+    def train_Model(self):
+        self.regr.fit(self.X, y)
+    
+
+    def predict(self, x):
+
+        newX = np.zeros(x.shape)
+
+        for i in range(2, self.degree + 1):
+            newX = np.concatenate((newX, x**i), axis = 1)
+        
+        return self.regr.predict(newX)
+
+    def score(self):
+        return self.regr.score(self.X, self.y)
+
+data = 'Data.txt'
+maxScore = -1
+bestDegree = 0
+
+for i in range(0, 15):
+    model = PolynomialRegression(i, data)
+    model.train_Model()
+    score = model.score()
+
+    if(score > maxScore):
+        maxScore = score
+        bestDegree = i
+    
+print(bestDegree)
+print(maxScore)
+
 
